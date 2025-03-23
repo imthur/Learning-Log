c4f1b4c1371b2a755c11c1a1a146d2fab7584594
Arthur <arthuraguiardev@gmail.com>
2025-03-22 23:43:29 -0300

    Adicionada função de deslogar/sair

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
-from django.contrib.auth import views as auth_views
+from django.urls import path # Importa a função path
+from django.contrib.auth import views as auth_views # Importa as views de autenticação do Django
+from . import views
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'), # Django fornece uma função de login que podemos chamar
+    path('logout/', views.logout_view, name='logout'), # Django fornece uma função de logout que podemos chamar
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect # Função que redireciona o usuário para uma página específica
+from django.urls import reverse # Função que gera URLs a partir de padrões de URL e parâmetros
+from django.contrib.auth import logout # Função de logout do Django
 
-# Create your views here.
+def logout_view(request):
+    """Encerra a sessão do usuário."""
+    logout(request) # Django fornece uma função de logout que podemos chamar
+    return HttpResponseRedirect(reverse('index')) # Redireciona o usuário para a página inicial do site
