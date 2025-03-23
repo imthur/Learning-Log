dcd8cd29b8ff16bb70af770ee23f15cb7eab0657
Arthur <arthuraguiardev@gmail.com>
2025-03-23 11:49:41 -0300

    Adicionada proteção às informações do usuário, permitindo que somente o dono dos tópicos consiga acessar ou editá-los.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render # Importa a função render
 from .models import Topic, Entry # Importa o modelo Topic do arquivo models.py
 from .forms import TopicForm, EntryForm # Importa o formulário TopicForm do arquivo forms.py
-from django.http import HttpResponseRedirect # Importa a função HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404 # Importa a função HttpResponseRedirect
 from django.urls import reverse # Importa a função reverse
 from django.contrib.auth.decorators import login_required
 
@@ -12,7 +12,7 @@ def index(request):
 @login_required
 def topics(request):
     """Mostra todos os tópicos."""
-    topics = Topic.objects.order_by('date_added') # Busca todos os objetos Topic e os ordena por data de criação / Topic é um modelo/tabela do banco de dados, criado em models.py
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added') # Busca todos os objetos Topic e os ordena por data de criação / Topic é um modelo/tabela do banco de dados, criado em models.py
     context = {'topics' : topics} # Cria um dicionário com a chave 'topics' e o valor topics, que contém todos os objetos da tabela Topic
     return render(request, 'learning_logs/topics.html', context) # Renderiza a página topics.html, passando o dicionário context [criado acima] como argumento
 
@@ -20,6 +20,9 @@ def topics(request):
 def topic(request, topic_id):
     """Mostra um único tópico e todas as suas entradas."""
     topic = Topic.objects.get(id=topic_id) # Busca um objeto Topic com o id passado como argumento
+    # Garante que o assunto pertence ao usuário atual
+    if topic.owner != request.user: # Verifica se o dono do tópico que está sendo acessado é o usuário atual
+        raise Http404 # Caso não seja, redireciona para uma página de erro padrão de Django
     entries = topic.entry_set.order_by('-date_added') # Busca todas as entradas relacionadas ao tópico e as ordena pela data de criação
     context = {'topic': topic, 'entries': entries} # Cria um dicionário com as chaves 'topic' e 'entries' e os valores topic e entries, respectivamente
     return render(request, 'learning_logs/topic.html', context) # Renderiza a página topic.html, passando o dicionário context como argumento
@@ -34,7 +37,9 @@ def new_topic(request):
         # Dados de POST submetidos; processa os dados
         form = TopicForm(data=request.POST) # Cria um formulário preenchido com os dados submetidos
         if form.is_valid(): # Se o formulário for válido
-            form.save() # Salva os dados do formulário no banco de dados
+            new_topic = form.save(commit=False) # Recebe os dados do formulário e armazena na variável new_topic
+            new_topic.owner = request.user # Define o usuário como dono do tópico que está sendo criado através do seu ID
+            new_topic.save() # Salva os dados recebidos pelo usuário, com o campo "owner" com o ID do usuário atualmente logado
             return HttpResponseRedirect(reverse('topics')) # Redireciona o usuário para a página topics.html, usando a função reverse para obter a URL de topics
 
     context = {'form': form} # Cria um dicionário com a chave 'form' e o valor form, que contém o formulário
@@ -44,6 +49,9 @@ def new_topic(request):
 def new_entry(request, topic_id):
     """Adiciona uma nova entrada para um tópico em particular."""
     topic = Topic.objects.get(id=topic_id) # Busca um objeto Topic com o id passado como argumento
+    # Garante que o assunto pertence ao usuário atual
+    if topic.owner != request.user: # Verifica se o dono do tópico que está sendo acessado é o usuário atual
+        raise Http404 # Caso não seja, redireciona para uma página de erro padrão de Django
     if request.method != 'POST': # Se a requisição não for do tipo POST
         # Nenhum dado submetido; cria um formulário em branco
         form = EntryForm() # Cria um formulário vazio
@@ -64,6 +72,8 @@ def edit_entry(request, entry_id):
     """Edita uma entrada existente."""
     entry = Entry.objects.get(id=entry_id) # Busca um objeto Entry com o id passado como argumento
     topic = entry.topic # Busca o tópico relacionado à entrada
+    if topic.owner != request.user: # Verifica se o dono do tópico que está sendo acessado é o usuário atual
+        raise Http404 # Caso não seja, redireciona para uma página de erro padrão de Django
     if request.method != 'POST': # Se a requisição não for do tipo POST
         # Requisição inicial; preenche o formulário com a entrada atual
         form = EntryForm(instance=entry) # Cria um formulário preenchido com os dados da entrada
