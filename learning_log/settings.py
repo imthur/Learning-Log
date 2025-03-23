402776af38d3d9333243e25e8546b2d0178ce490
Arthur <arthuraguiardev@gmail.com>
2025-03-23 13:06:16 -0300

    Feita integração com bootstrap e iniciada estilização das páginas

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'learning_logs',
     'users',
+    
+    # Apps de terceiros
+    
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +131,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # URL login page
 
 LOGIN_URL = '/users/login'
+
+# Bootstrap3
+
+BOOTSTRAP3 = {
+    'include_jquery' : True,
+}
